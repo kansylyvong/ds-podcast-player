eb1fb3730d6b8777959255c740e1efe29b3721fa
kvansylyvong <kevin.vansylyvong@zoominfo.com>
2023-10-11 10:18:53 -0400

    added filtering on host and level

--- src/app/podcasts/podcast-list.component.ts
+++ src/app/podcasts/podcast-list.component.ts
@@ -17,8 +17,16 @@ export class PodcastListComponent implements OnInit, OnDestroy {
 
   levels$ = of(['All','Superbeginner','Beginner','Intermediate','Advanced']);
 
+  hosts$ = of(['All','Abraham','Adam','Adrià','Agustina','Aitana','Alan','Alexandra','Alfonso','Alfredo','Alma','Amey','Analía','Andrea','Andrés H','Bernardo','Betsy','Betsy C','Bianncka','Camilo','Carolina','César','Claudia','David & Adrián','Débora','Edgar','Edwin','Elías','Enrique','Fátima','How to Spanish','Ian','Isabel','Iván','Javier','Jose María','Jostin','Juan','Judit','Karlos','Laura','Lorena','Lorena O','Marce','María','María del Mar','Marifer','Marinés','Mariona','Mauricio','Maximiliano','Michelle','Montserrat','Nacho','Natalia','Núria','Pablo','Pablo\'s mom','Pati','Pilar','Rafael','Ramón','Ricardo','Ricardo R','Rocío','Sandra','Sergio','Sofía','Sonia & Alberto','Tamara','Tere','Tomás','Toni','Valeria','Victoria'])
+
+
   private levelSelectedSubject = new BehaviorSubject<string>('All');
+
+  private levelSelectedHostSubject = new BehaviorSubject<string>('All');
+
+  levelSelectedHostAction$ = this.levelSelectedHostSubject.asObservable();
   levelSelectedAction$ = this.levelSelectedSubject.asObservable();
+
   pageTitle: string = 'Podcasts List';
   showDescription: boolean = false;
   podcasts$: Observable<IPodcast[]> = this.podcastService.podcasts$.pipe(
@@ -29,25 +37,22 @@ export class PodcastListComponent implements OnInit, OnDestroy {
   );
   filteredPodcasts$ = combineLatest([
     this.podcasts$,
-    this.levelSelectedAction$
+    this.levelSelectedAction$,
+    this.levelSelectedHostAction$
   ]).pipe(
     tap(levelSelectedAction => console.log(levelSelectedAction)),
-    map(([podcasts, selectedLevel]) =>
+    map(([podcasts, selectedLevel, selectedHost]) =>
       podcasts.filter((podcast) =>
       selectedLevel == 'All' ? true : podcast.title.includes(selectedLevel))
+      .filter((podcast) =>
+      selectedHost == 'All' ? true : podcast.description.includes(selectedHost)
+      )
     ),
     catchError( err => {
       this.errorMessage = err;
       return EMPTY;
     })
   )
-  //this.podcasts$;
-  hosts$ = this.podcasts$.pipe(
-    mergeAll(),
-    distinct(v => v.host),
-    map( x => x.host),
-    toArray(),
-  );
 
   currentPodcast: any = {};
   private _listFilter = '';
@@ -81,9 +86,15 @@ export class PodcastListComponent implements OnInit, OnDestroy {
     ))
   }
 
-  onSelected(level: MatSelectChange) {
+  onSelectedLevel(level: MatSelectChange) {
     console.log(level);
     this.levelSelectedSubject.next(level.value);
+
+  }
+
+  onSelectedHost(level: MatSelectChange) {
+    console.log(level);
+    this.levelSelectedHostSubject.next(level.value);
 
   }
   openFile(podcast : IPodcast, index: number) : void {
